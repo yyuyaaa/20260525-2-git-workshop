9fd76b32572f521586a8bd65e6c493cab315e8de
yyuyaaa <241205162@ccmailg.meijo-u.ac.jp>
2026-05-25 16:05:28 +0900

    全てyellowに変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'
+    'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
