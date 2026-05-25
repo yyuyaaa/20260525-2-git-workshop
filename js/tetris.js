e1ed04ee85f61643a74032123c8d63707026470e
yyuyaaa <241205162@ccmailg.meijo-u.ac.jp>
2026-05-25 15:12:03 +0900

    全て緑に変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+    'green', 'green', 'green', 'green', 'green', 'green', 'green'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
